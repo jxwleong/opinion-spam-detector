81989ead4d670617d5094c182a5ae82c7801655e
Leong, Jason Xie Wei <jason.xie.wei.leong@intel.com>
2023-04-19 14:20:26 +0800

    Update html

--- front_end.py
+++ front_end.py
@@ -1,5 +1,21 @@
 from flask import Flask, render_template, request
+import pickle
+import json
+# Need to import the pickled object
+# else when load will have this problem
+# AttributeError: Can't get attribute 'OpinionSpamDetectorModel' on <module '__main__' from '.\\main.py'>
+from model_training import OpinionSpamDetectorModel
+# load the model from disk
+filename = 'finalized_model.bin'
+with open(filename, 'rb') as f:
+    loaded_model = pickle.load(f)
 
+
+
+mapping = {
+    "OR": "Authentic",
+    "CG": "Spam"
+}
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -16,7 +32,17 @@ def index():
 def process_text():
     text = request.form['text']
     # Do something with the text here
-    response = 'You entered: ' + text
+
+    new_review = text
+    new_review_vect = loaded_model.vectorizer.transform([new_review])
+    new_review_pred = loaded_model.model.predict(new_review_vect)
+    print("Prediction for new review:", new_review_pred[0])
+    response_data = {
+        "review": text,
+        "outcome": new_review_pred[0],
+        "result": mapping[new_review_pred[0]]
+    }
+    response = json.dumps(response_data, indent=4)
     return render_template('index.html', response=response)
 
 
