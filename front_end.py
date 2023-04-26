e15bbcf768c292ddf92b301ef416c0dfa4e60c32
Leong, Jason Xie Wei <jason.xie.wei.leong@intel.com>
2023-04-26 21:53:46 +0800

    Fixed conflict

--- front_end.py
+++ front_end.py
@@ -1,31 +1,14 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request, jsonify
-=======
-from flask import Flask, render_template, request
->>>>>>> 1f0e28d062008dcd2f93430d89ee09589882e665
 import pickle
 import json
 import os 
 
-<<<<<<< HEAD
-=======
-# Need to import the pickled object
-# else when load will have this problem
-# AttributeError: Can't get attribute 'OpinionSpamDetectorModel' on <module '__main__' from '.\\main.py'>
-from model_training import OpinionSpamDetectorModel
-# load the model from disk
-filename = os.path.join(os.path.dirname(__file__), 'finalized_model.bin')
-with open(filename, 'rb') as f:
-    loaded_model = pickle.load(f)
-
->>>>>>> 1f0e28d062008dcd2f93430d89ee09589882e665
 
 
 mapping = {
     "OR": "Authentic",
     "CG": "Spam"
 }
-<<<<<<< HEAD
 
 
 model_path = {
@@ -132,37 +115,4 @@ def get_model_metrics():
         return render_template("index.html", response=formatted_metrics, model_name=current_model_name, model=model, model_path=model_path)
 
 if __name__ == "__main__":
-=======
-app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        text = request.form['text']
-        # Do something with the text here
-        response = 'You entered: ' + text
-        return render_template('index.html', response=response)
-    else:
-        return render_template('index.html')
-    
-@app.route('/process_text', methods=['POST'])
-def process_text():
-    text = request.form['text']
-    # Do something with the text here
-
-    new_review = text
-    new_review_vect = loaded_model.vectorizer.transform([new_review])
-    new_review_pred = loaded_model.model.predict(new_review_vect)
-    print("Prediction for new review:", new_review_pred[0])
-    response_data = {
-        "review": text,
-        "outcome": new_review_pred[0],
-        "result": mapping[new_review_pred[0]]
-    }
-    response = json.dumps(response_data, indent=4)
-    return render_template('index.html', response=response)
-
-
-if __name__ == '__main__':
->>>>>>> 1f0e28d062008dcd2f93430d89ee09589882e665
     app.run(debug=True)
--- model_training.py
+++ model_training.py
@@ -3,11 +3,8 @@ __author__ = ["jxwleong"]
 import os
 import sys 
 import pickle
-<<<<<<< HEAD
 import logging 
 import json
-=======
->>>>>>> 1f0e28d062008dcd2f93430d89ee09589882e665
 
 REPO_ROOT= os.path.normpath(os.path.join(os.path.abspath(__file__), ".."))
 LIB_PATH = os.path.join(REPO_ROOT, "lib")
@@ -19,7 +16,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-<<<<<<< HEAD
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -51,10 +47,6 @@ logger.addHandler(file_handler)
 
 model_metrics = {}
 model_bin_name_prefix = "final_trained_model_"
-=======
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
->>>>>>> 1f0e28d062008dcd2f93430d89ee09589882e665
 class OpinionSpamDetectorModel:
     def __init__(self, dataset_path=DATASET_PATH):
         self.dataset_path = dataset_path
@@ -65,11 +57,7 @@ class OpinionSpamDetectorModel:
 
         self.x = self.df["text_"]
         self.y = self.df["label"]
-<<<<<<< HEAD
         logger.info(self.y)
-=======
-        print(self.y)
->>>>>>> 1f0e28d062008dcd2f93430d89ee09589882e665
         # Split data into training and testing sets
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.x, self.y, test_size=0.2, random_state=42)
 
@@ -89,7 +77,6 @@ class OpinionSpamDetectorModel:
         self.recall = recall_score(self.y_test, self.y_pred, average='micro')
         self.f1 = f1_score(self.y_test, self.y_pred, average='micro')
 
-<<<<<<< HEAD
         logger.info("Accuracy:", self.accuracy)
         logger.info("Precision:", self.precision)
         logger.info("Recall:", self.recall)
@@ -98,21 +85,12 @@ class OpinionSpamDetectorModel:
 
 if __name__ == "__main__":
     """
-=======
-        print("Accuracy:", self.accuracy)
-        print("Precision:", self.precision)
-        print("Recall:", self.recall)
-        print("F1 score:", self.f1)
-
-if __name__ == "__main__":
->>>>>>> 1f0e28d062008dcd2f93430d89ee09589882e665
     my_model = OpinionSpamDetectorModel()
     my_model.train()
     # Use model to predict new reviews
     new_review = "This product really helps to make the cooking easier!"
     new_review_vect = my_model.vectorizer.transform([new_review])
     new_review_pred = my_model.model.predict(new_review_vect)
-<<<<<<< HEAD
     logger.info("Prediction for new review:", new_review_pred[0])
 
     # https://machinelearningmastery.com/save-load-machine-learning-models-python-scikit-learn/
@@ -209,13 +187,3 @@ if __name__ == "__main__":
         pickle.dump((lr_model, vectorizer), f)
     """
  
-=======
-    print("Prediction for new review:", new_review_pred[0])
-
-    # https://machinelearningmastery.com/save-load-machine-learning-models-python-scikit-learn/
-    # Save the model to use later.
-    filename = filename = os.path.join(os.path.dirname(__file__), 'finalized_model.bin')
-    with open(filename, 'wb') as f:
-        pickle.dump(my_model, f)
- 
->>>>>>> 1f0e28d062008dcd2f93430d89ee09589882e665
